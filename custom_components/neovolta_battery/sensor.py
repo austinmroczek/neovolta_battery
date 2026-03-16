fed8734a2b4db6115ad8a17e2ff42d32b8bd7470
Austin Mroczek <austin@mroczek.org>
2026-03-15 21:40:58 -0700

    fix imports

--- custom_components/neovolta_battery/sensor.py
+++ custom_components/neovolta_battery/sensor.py
@@ -15,6 +15,8 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     PERCENTAGE,
+    UnitOfEnergy,
+    UnitOfPower,
     UnitOfTemperature,
 )
 from homeassistant.core import HomeAssistant
